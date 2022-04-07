93c7a5a0f8259a68e82fdb8a85066d8f922a5df5
Kaemi <51827723+Camilla0619@users.noreply.github.com>
2022-04-08 04:41:05 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,4 @@
+//server
 use std::io::{Error, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
@@ -38,4 +39,27 @@ fn main() -> std::io::Result<()> {
 
     Ok(())
 }
+
+
+//client
+
+use std::io::{self,prelude::*,BufReader,Write};
+use std::net::TcpStream;
+use std::str;
+ 
+ 
+fn main() -> io::Result<( )> {
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
+    for _ in 0..1000 {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read");
+        stream.write(input.as_bytes()).expect("failed to write");
+ 
+        let mut reader =BufReader::new(&stream);
+        let mut buffer: Vec<u8> = Vec::new();
+        reader.read_until(b'\n',&mut buffer)?;
+        println!("read form server:{}",str::from_utf8(&buffer).unwrap());
+        println!("");
+ 
+    }
 
