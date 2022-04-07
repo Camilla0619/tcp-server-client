a86659373797ca4c694b4b1211a761421d5f143c
Kaemi <51827723+Camilla0619@users.noreply.github.com>
2022-04-08 04:38:51 +0800

    client

--- main.rs
+++ main.rs
@@ -1,41 +1,27 @@
-use std::io::{Error, Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::time;
-
-fn handle_client(mut stream: TcpStream) -> Result<(), Error>{
-    let mut buf = [0; 512];
-    for _ in 0..1000 {
-        let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0 {
-            return Ok(());
-        }
-
-        stream.write(&buf[..bytes_read])?;
-        thread::sleep(time::Duration::from_secs(1 as u64));
-    }
-
-    Ok(())
-}
+use std::io::{self, prelude::*, BufReader, Write};
+use std::net::TcpStream;
+use std::str;
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+    for _ in 0..10 {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read from stdin");
+        stream
+            .write(input.as_bytes())
+            .expect("Failed to write to stream");
 
-    for stream in listener.incoming() {
-        let stream = stream.expect("failed!");
-        let handle = thread::spawn(move || {
-            handle_client(stream)
-        .unwrap_or_else(|error| eprintln!("{:?}", error));
-        });
-
-        thread_vec.push(handle);
+        let mut reader = BufReader::new(&stream);
+        let mut buffer: Vec<u8> = Vec::new();
+        reader
+            .read_until(b'\n', &mut buffer)
+            .expect("Could not read into buffer");
+        println!("{}", 
+            str::from_utf8(&buffer).expect("Could not write buffer as string"));
+        println!("");
     }
-
-    for handle in thread_vec {
-        handle.join().unwrap();
-    }
-
     Ok(())
 }
 
